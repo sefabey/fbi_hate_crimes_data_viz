711c3c0053082f804b768d5619fc6a3424461ba1
Sefa <sefabey@gmail.com>
2018-07-10 22:35:31 +0000

    update geocode csv

--- src/001_EDA.R
+++ src/001_EDA.R
@@ -9,3 +9,33 @@ library(ggmap)
 reported_agency %>% 
   mutate(agency_with_state= paste(Agency_name, State, sep = ", ")) %>% #paste state names to agencies
   mutate_geocode(agency_with_state, sensor=F) -> reported_agency_geog
+
+
+reported_agency_geog$lat %>% is.na() %>% table()
+
+reported_agency_geog %>% 
+  filter(is.na(lat)) %>% 
+  mutate_geocode(agency_with_state, sensor=F)-> reported_agency_geog_2
+
+reported_agency_geog_2 <- reported_agency_geog_2 %>% 
+  mutate(lon=lon.1) %>% 
+  mutate(lat=lat.1) %>% 
+  select(-lon.1, -lat.1)
+
+reported_agency_geog_2 %>% 
+  filter(is.na(lat)) %>% 
+  select(-lon, -lat) %>% 
+  mutate_geocode(agency_with_state, sensor=F)-> reported_agency_geog_3
+
+reported_agency_geog_3 %>% 
+  filter(is.na(lat)) %>% 
+  select(-lon, -lat) %>% 
+  mutate_geocode(agency_with_state, sensor=F) -> reported_agency_geog_4
+
+reported_agency_geog %>%
+  left_join(reported_agency_geog_2) %>%
+  left_join(reported_agency_geog_3)%>%
+  left_join(reported_agency_geog_4)-> reported_agency_geo_complete
+
+reported_agency_geo_complete %>% write_csv("data/Table_13_geo_002.csv")
+
