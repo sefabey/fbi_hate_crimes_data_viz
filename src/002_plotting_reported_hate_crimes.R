6e18f2d1168ebe4083ae65b2967e709727ae3bfb
Sefa <sefabey@gmail.com>
2018-07-15 10:13:58 +0000

    add a cleaner map

--- src/002_plotting_reported_hate_crimes.R
+++ src/002_plotting_reported_hate_crimes.R
@@ -7,6 +7,7 @@ library(hrbrthemes)
 # install.packages("ggmap")
 # devtools:: install_version("ggplot2", version = "2.2.1", repos = "http://cran.us.r-project.org")
 # install.packages("ggplot2")
+reported_agency_geo_complete <- read_csv('data/Table_13_geo_003.csv')
 
 reported_agency_geo_complete_2 <- reported_agency_geo_complete%>% 
   filter(State!='Alaska') %>% 
@@ -18,21 +19,20 @@ reported_agency_geo_complete %>% names
 
 ggmap(usa_big) + 
   geom_point(data = reported_agency_geo_complete_2, 
-             mapping = aes(x = lon, y = lat, size = Total_incidents_Q1_to_Q4, color=Agency_Type))+
+             mapping = aes(x = lon, y = lat, size = Total_incidents_Q1_to_Q4))+
   # scale_size_area()+
-  scale_radius(range=c(0.1, 5))+
+  scale_radius(range=c(0.05, 5))+
   scale_color_viridis_d()+
-  theme_minimal()+
+  labs(x="Longitude", y="Latitude",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2016",
+       subtitle="Source: FBI Hate Crime Statistics",
+       caption="Social Data Science Lab, Cardiff University") +
+  theme_ipsum_rc()+
+  theme(legend.position="bottom",
+        legend.direction = "horizontal",
+        legend.title = element_text( size=14),
+        plot.caption= element_text(size=14))+
+  labs(size="Legend")+
   NULL
 
-
-us_map <- get_map('the US', maptype = 'terrain-background')
-
-ggplot(temp_map)
-
-us.map <- get_map(location=c(lon=-100, lat=40), zoom=4, maptype="terrain", filename="data/ggmapTemp")  
-p <- ggmap(us.map) +  
-  geom_point(data=reported_agency_geo_complete, aes(x=lon, y=lat, size=Bias_Motivation_Race_Ethnicity_Ancestry/10000))  
-
-.libPaths()
-
+ggsave(filename = 'viz/reported_hate_crimes.png', device = "png", scale = 2, dpi = 'retina', width = 20, height = 12, units = "cm")
