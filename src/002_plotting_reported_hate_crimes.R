5489f8173bbf52fb7e1c81ba89e12fd4ae762f90
Sefa <sefabey@gmail.com>
2018-07-15 10:27:43 +0000

    add map 3 (albers projection)

--- src/002_plotting_reported_hate_crimes.R
+++ src/002_plotting_reported_hate_crimes.R
@@ -36,3 +36,76 @@ ggmap(usa_big) +
   NULL
 
 ggsave(filename = 'viz/reported_hate_crimes.png', device = "png", scale = 2, dpi = 'retina', width = 20, height = 12, units = "cm")
+
+# Above plot was too busy. Replotting with an empty plot.
+
+
+# plot 2: less clutter =====
+
+us <- map_data("state")
+
+ggplot()+
+  geom_map(data = us, map=us,
+           aes(x=long, y=lat, map_id=region),
+           fill="#CCCC99", color="#666633", size=0.15)+
+  geom_point(data = reported_agency_geo_complete_2, 
+             mapping = aes(x = lon, y = lat, size = Total_incidents_Q1_to_Q4))+
+  scale_radius(range=c(0.01, 7))+
+  scale_alpha()+
+  labs(x="Longitude", y="Latitude",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2016",
+       subtitle="Source: FBI Hate Crime Statistics",
+       caption="Social Data Science Lab, Cardiff University") +
+  
+  theme_ipsum_rc()+
+  theme(legend.position="bottom",
+        legend.direction = "horizontal",
+        legend.title = element_text( size=12),
+        plot.caption= element_text(size=14),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank()
+        
+        )+
+  labs(size="Number of Hate Crimes")+
+  NULL
+
+ggsave(filename = 'viz/reported_hate_crimes_ver2.png', device = "png", scale = 2, dpi = 600, width = 20, height = 12, units = "cm")
+
+
+# plot 3: less clutter, albers projection =====
+
+
+ggplot()+
+  geom_map(data = us, map=us,
+           aes(x=long, y=lat, map_id=region),
+           fill="#CCCC99", color="#666633", size=0.25)+
+  coord_map("albers",lat0=39, lat1=45)+
+  
+  geom_point(data = reported_agency_geo_complete_2, 
+             mapping = aes(x = lon, y = lat, size = Total_incidents_Q1_to_Q4))+
+  scale_radius(range=c(0.01, 7))+
+  scale_alpha()+
+  labs(x="Longitude", y="Latitude",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2016",
+       subtitle="Source: FBI Hate Crime Statistics",
+       caption="Social Data Science Lab, Cardiff University") +
+  
+  theme_ipsum_rc()+
+  theme(legend.position="bottom",
+        legend.direction = "horizontal",
+        legend.title = element_text( size=12),
+        plot.caption= element_text(size=14),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank()
+        
+  )+
+  labs(size="Number of Hate Crimes")+
+  NULL
+
+ggsave(filename = 'viz/reported_hate_crimes_ver3.png', device = "png", scale = 2, dpi = 600, width = 20, height = 12, units = "cm")
+
+
