52aee5f88d74c10238442388bdc689e9df08cb8e
Sefa Ozalp <sefabey@gmail.com>
2020-01-27 07:23:15 +0000

    add chropleth, fix titles

--- src/src_2018/003_plotting_reported_crimes.R
+++ src/src_2018/003_plotting_reported_crimes.R
@@ -14,7 +14,7 @@ reported_agency_geo_complete %>%
 
 
 
-reported_agency_geo_complete_2 <- reported_agency_geo_complete%>% 
+reported_agency_geo_complete_2 <- reported_agency_geo_complete %>% 
   filter(!State %in% c('Alaska', "Hawaii")) %>% 
   filter(Population>10000 | is.na(Population)) #%>%  # NAs are metropolitan counties. They should be included.
 
@@ -29,7 +29,7 @@ plot_ggmap <-  ggmap(usa_big) +
   scale_radius(range=c(0.05, 5))+
   scale_color_viridis_d()+
   labs(x="Longitude", y="Latitude",
-       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2016",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2018",
        subtitle="Source: FBI Hate Crime Statistics",
        caption="Social Data Science Lab, Cardiff University") +
   theme_ipsum_rc()+
@@ -96,7 +96,7 @@ plot_albers <- ggplot()+
   scale_radius(range=c(0.01, 4))+
   scale_alpha()+
   labs(x="Longitude", y="Latitude",
-       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2016",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2018",
        subtitle="Source: FBI Hate Crime Statistics",
        caption="HateLab, 2020, by @SefaOzalp") +
   
@@ -115,3 +115,49 @@ plot_albers <- ggplot()+
   NULL
 
 ggsave(plot_albers, filename = 'viz/viz_2018/reported_hate_crimes_albers.pdf', device = cairo_pdf, scale=1.8, dpi = 500, width = 20, height = 12, units = "cm")
+
+
+# plot 4: choropleth of states =====
+
+library(usmap) # includes alaska and Hawaii
+
+# plot_usmap("states")
+# state_map <- us_map(regions = "states")
+# str(state_map)
+# state_map
+
+dt_fbi_hatecrimes <- reported_agency_geo_complete %>% 
+  group_by(State) %>% 
+  summarise(total_incidents_state=sum(Total_incidents_Q1_to_Q4)) %>% view()
+  mutate(fips=usmap::fips(State)) %>% 
+  arrange(fips)
+
+statepop %>% head()
+dt_fbi_hatecrimes %>% head()
+
+plot_state_choropleth <- plot_usmap(data = dt_fbi_hatecrimes, 
+           values = "total_incidents_state" , color = "black") + 
+  scale_fill_gradientn (
+                        colours=rev(RColorBrewer::brewer.pal(8,"Spectral")),
+                        # na.value="white",
+                        na.value = "grey90",
+                        guide = "colourbar")+
+  labs(x="Longitude", y="Latitude",
+       title="Hate Crimes Reported by the US Law Enforcement Agencies in 2018",
+       subtitle="Source: FBI Hate Crime Statistics",
+       caption="HateLab, 2020, by @SefaOzalp") +
+  theme_ipsum_rc()+
+  theme(legend.position="right",
+        legend.direction = "vertical",
+        legend.title = element_text( size=12),
+        plot.caption= element_text(size=14),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank()
+        
+  )+
+  labs(size="Number of Hate Incidents")+
+  NULL
+
+ggsave(plot_state_choropleth, filename = 'viz/viz_2018/reported_state_choropleth.pdf', device = cairo_pdf, scale=1.8, dpi = 500, width = 20, height = 12, units = "cm")
